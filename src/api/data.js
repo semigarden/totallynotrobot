b2aff34212b8e611e2d26968042538c5de722fdf
semigarden <semigardenn@gmail.com>
2025-07-19 18:02:02 +0400

    update data

--- src/api/data.js
+++ src/api/data.js
@@ -25,18 +25,14 @@ const data = {
         {category: "0", label: "HTML", icon: faHtml5, iconType: "fontawesome", animation: "fa-beat-fade", color: "#EF8354"},
         {category: "0", label: "JavaScript", icon: faJs, iconType: "fontawesome", animation: "fa-beat-fade", color: "#FDE74C"},
         {category: "0", label: "CSS", icon: faCss3Alt, iconType: "fontawesome", animation: "fa-beat-fade", color: "#63ADF2"},
-        // {category: "0", label: "Figma", icon: faFigma, iconType: "fontawesome", animation: "fa-fade", color: "#F0C36B"},
-
         {category: "0", label: "Node.js", icon: faNode, iconType: "fontawesome", animation: "fa-fade", color: "#40F99B"},
         {category: "0", label: "Laravel", icon: faLaravel, iconType: "fontawesome", animation: "fa-fade", color: "#FF6F7A"},
         {category: "0", label: "PHP", icon: faPhp, iconType: "fontawesome", animation: "fa-fade", color: "#9B5DE5"},
         {category: "0", label: "Python", icon: faPython, iconType: "fontawesome", animation: "fa-fade", color: "#FDE74C"},
         {category: "0", label: "Github", icon: faGithubAlt, iconType: "fontawesome", animation: "fa-fade", color: "#fafbfc"},
-        // {category: "0", label: "Tumblr", icon: faTumblrSquare, iconType: "fontawesome", animation: "fa-fade", color: "#fafbfc"},
         {category: "0", label: "Reddit", icon: faRedditAlien, iconType: "fontawesome", animation: "fa-fade", color: "orangered"},
         {category: "0", label: "Mastodon", icon: faMastodon, iconType: "fontawesome", animation: "fa-fade", color: "#6364ff"},
         {category: "0", label: "Monero", icon: faMonero, iconType: "fontawesome", animation: "fa-fade", color: "#5933ad"},
-        // {category: "0", label: "Instagram", icon: faSquareInstagram, iconType: "fontawesome", animation: "fa-fade", color: "#d0006d"},
     ]
 }
 
